176031009e639cda57e858012e8ad9d65e972960
AntonAA <AntonAAMain@yandex.ru>
2024-09-29 14:44:41 +0300

    removed comments

--- src/shared/redux/store.ts
+++ src/shared/redux/store.ts
@@ -5,15 +5,11 @@ import todoSlice from "./slices/todoSlice";
 export const store = configureStore({
   reducer: {
     todo: todoSlice,
-    // posts: postsReducer,
-    // comments: commentsReducer,
-    // users: usersReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
